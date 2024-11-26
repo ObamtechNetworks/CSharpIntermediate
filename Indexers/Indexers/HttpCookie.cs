0aeb5198027e44c9309174f312ef1e942e8a9456
Ipadeola Michael Bamidele <ipadeolamichaelbamidele@gmail.com>
2024-11-26 23:25:11 +0100

    indexers in c#

---
+++ Indexers/Indexers/HttpCookie.cs
@@ -0,0 +1,21 @@
+﻿namespace Indexers;
+
+public class HttpCookie
+{
+    // since we are dealing with keyvalue pairs, dictionaries would be best data structure to use
+    private readonly Dictionary<string, string> _dictionary;
+
+    // create a constructor to initialize empty dictionary
+    public HttpCookie()
+    {
+        _dictionary = new Dictionary<string, string>();
+    }
+
+    // an indexer is created using the this keyword
+    public string this[string key]
+    {
+        get {return _dictionary[key]; }
+        set { _dictionary[key] = value; }
+    }
+    
+}
---
+++ Indexers/Indexers/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Indexers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookie = new HttpCookie();
+            cookie["name"] = "Michael";
+
+            Console.WriteLine(cookie["name"]);
+        }
+    }
+}
